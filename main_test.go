c31b2da63831b58cb31f44293e97660bb08c0577
Maksym Prokopov <mprokopov@gmail.com>
2020-05-04 09:37:32 +0200

    fixed failing test

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSimpleFactory(t *testing.T) {
-	f := SimpleFactory("http://localhost1")
+	f := SimpleFactory("http://localhost")
 
 	if f.Url != "http://localhost" {
 		t.Errorf("feature incorrect, got %s, want: %s ", f.Url, "http://localhost")
